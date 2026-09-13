4908f7dbd13e5e9ccc8cd8ee1fce551ab46f30d5
limetortillaship <shanonps@icloud.com>
2026-09-13 10:20:24 -0400

    Minor changes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,30 +13,32 @@ function DevCard({ card }) {
     <article className={styles.selx} data-card="dev">
       <div data-card="title">
         <div data-card="icon">
-          <UserCircleIcon size={16} weight="fill" />
+          <UserCircleIcon size={14} weight="fill" />
         </div>
         <span data-card="type">dev</span>
       </div>
-      <div data-card="inner" id={styles.dev}>
-        <div data-card="dev-icon">
-          <div>
-            <img src={card.photo} alt="dev pic is pixelated" />
+      <article data-card="body">
+        <div data-card="inner" id={styles.dev}>
+          <div data-card="dev-icon">
+            <div>
+              <img src={card.photo} alt="dev pic is pixelated" />
+            </div>
+          </div>
+          <div data-card="dev-intro">
+            <div data-card="intro">
+              <span data-card="intro-icon">
+                <TerminalIcon size={14} weight="bold" />
+              </span>
+              <span data-card="intro-title">
+                <h2>{card.title}</h2>
+              </span>
+            </div>
+            <div data-card="desc">
+              <p>{compiler(card.desc)}</p>
+            </div>
           </div>
         </div>
-        <div data-card="dev-intro">
-          <div data-card="intro">
-            <span data-card="intro-icon">
-              <TerminalIcon size={16} weight="bold" />
-            </span>
-            <span data-card="intro-title">
-              <h2>{card.title}</h2>
-            </span>
-          </div>
-          <div data-card="desc">
-            <p>{compiler(card.desc)}</p>
-          </div>
-        </div>
-      </div>
+      </article>
     </article>
   );
 }
@@ -46,25 +48,27 @@ function PostCard({ card }) {
     <article className={styles.selx} data-card="post">
       <div data-card="title">
         <div data-card="icon">
-          <NoteIcon size={16} weight="fill" />
+          <NoteIcon size={14} weight="fill" />
         </div>
         <span data-card="type">post</span>
       </div>
-      <div data-card="inner" id={styles.post}>
-        <div data-card="post-title">
-          <span>
-            <h4>{card.title}</h4>
-          </span>
-        </div>
-        <div data-card="post-body">
-          <p>
-            {compiler(card.body[0])}...
+      <article data-card="body">
+        <div data-card="inner" id={styles.post}>
+          <div data-card="post-title">
             <span>
-              <a href="/blog">Read More</a>
+              <h4>{card.title}</h4>
             </span>
-          </p>
+          </div>
+          <div data-card="post-body">
+            <p>
+              {compiler(card.body[0])}...
+              <span>
+                <a href="/blog">Read More</a>
+              </span>
+            </p>
+          </div>
         </div>
-      </div>
+      </article>
     </article>
   );
 }
@@ -74,21 +78,23 @@ function PhotoCard({ card }) {
     <article className={styles.selx} data-card="photo">
       <div data-card="title">
         <div data-card="icon">
-          <ImageSquareIcon size={16} weight="fill" />
+          <ImageSquareIcon size={14} weight="fill" />
         </div>
         <span data-card="type">photo</span>
       </div>
-      <div data-card="inner" id={styles.photo}>
-        <div data-card="img">
-          <img src={card.url} />
+      <article data-card="body">
+        <div data-card="inner" id={styles.photo}>
+          <div data-card="img">
+            <img src={card.url} />
+          </div>
+          <div data-card="img-caption">
+            <p>
+              Placerat in id cursus mi pretium tellus duis. Pretium tellus duis
+              convallis tempus leo eu aenean.
+            </p>
+          </div>
         </div>
-        <div data-card="img-caption">
-          <p>
-            Placerat in id cursus mi pretium tellus duis. Pretium tellus duis
-            convallis tempus leo eu aenean.
-          </p>
-        </div>
-      </div>
+      </article>
     </article>
   );
 }
@@ -104,30 +110,32 @@ function ProjectsCard({ card }) {
     <article className={styles.selx} data-card="projects">
       <div data-card="title">
         <div data-card="icon">
-          <CodeBlockIcon size={16} weight="fill" />
+          <CodeBlockIcon size={14} weight="fill" />
         </div>
         <div data-card="type">projects</div>
       </div>
-      <div data-card="inner" id={styles.projs}>
-        <div data-card="project-links">
-          {card.links.map((proj, i) => {
-            let projectLink = Object.entries(proj)[0];
-            //console.log(projectLink);
-            return (
-              <div className="project" key={i}>
-                <span
-                  data-card="project-title"
-                  id="project-link"
-                  onClick={handleClick}
-                >
-                  <a href={projectLink[1]}>{projectLink[0]}</a>
-                </span>
-                <span data-card=""></span>
-              </div>
-            );
-          })}
+      <article data-card="body">
+        <div data-card="inner" id={styles.projs}>
+          <div data-card="project-links">
+            {card.links.map((proj, i) => {
+              let projectLink = Object.entries(proj)[0];
+              //console.log(projectLink);
+              return (
+                <div className="project" key={i}>
+                  <span
+                    data-card="project-title"
+                    id="project-link"
+                    onClick={handleClick}
+                  >
+                    <a href={projectLink[1]}>{projectLink[0]}</a>
+                  </span>
+                  <span data-card=""></span>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      </article>
     </article>
   );
 }
